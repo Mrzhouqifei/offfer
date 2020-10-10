158242ee76e6d0bbbfb8698ec422c4b26beecb53
Mrzhouqifei <18401620071@163.com>
2020-10-10 20:22:37 +0800

    init

---
+++ 常考面试/排序链表队列/topK大的数.py
@@ -0,0 +1,28 @@
+"""
+leetcode 215
+改进的快排法
+"""
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+
+        def quickSort(nums, k):
+            pivot = nums[len(nums) // 2]
+            left, mid, right = [], [], []
+
+            for x in nums:
+                if x < pivot:
+                    left.append(x)
+                elif x == pivot:
+                    mid.append(x)
+                else:
+                    right.append(x)
+
+            if len(left) >= k:
+                res = quickSort(left, k)
+            elif len(left) + len(mid) >= k:
+                res = mid[0]
+            else:
+                res = quickSort(right, k - len(left) - len(mid))
+            return res
+
+        return quickSort(nums, len(nums) - k + 1)
