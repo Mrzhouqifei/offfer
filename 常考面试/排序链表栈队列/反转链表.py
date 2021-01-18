be0f7872089146bc730e1886711d13a5f62d579e
Mrzhouqifei <18401620071@163.com>
2021-01-18 16:21:52 +0800

    init

--- 常考面试/排序链表栈队列/反转链表.py
+++ 常考面试/排序链表栈队列/反转链表.py
@@ -4,8 +4,6 @@
 #         self.val = x
 #         self.next = None
 
-# 栈的压入弹出序列
-
 class Solution:
     def reverseList(self, head):
         """
@@ -21,3 +19,45 @@ class Solution:
             pre = p
             p = next
         return pre
+
+"""
+92. 反转链表 II
+反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
+说明:
+1 ≤m≤n≤ 链表长度。
+
+示例:
+输入: 1->2->3->4->5->NULL, m = 2, n = 4
+输出: 1->4->3->2->5->NULL
+"""
+
+class Solution:
+    def reverseBetween(self, head: ListNode, m: int, n: int) -> ListNode:
+        reverse_m = head
+        i = 1
+
+        while i < m:
+            reverse_m = reverse_m.next
+            i += 1
+
+        pre, p = None, reverse_m
+        while i <= n:
+            next_tmp = p.next
+            p.next = pre
+            pre = p
+            p = next_tmp
+            i += 1
+
+        reverse_m.next = p
+        new_head = pre
+
+        if m == 1:
+            return new_head
+        else:
+            i = 1
+            p = head
+            while i < m - 1:
+                p = p.next
+                i += 1
+            p.next = new_head
+            return head
